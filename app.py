4d5515e791a12285cbe294d4470c54e24d96b1b5
Austin <133173997+4-Walker@users.noreply.github.com>
2026-05-31 11:07:27 +1000

    Sentiment analysis call and visualization
    Added sentiment analysis functionality and visualization

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from textblob import TextBlob
 from clean_data import clean_data
+from sentiment import analyse_sentiment
 
 st.title("Customer Sentiment and Theme Dashboard")
 
@@ -18,6 +19,7 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     before_cleaning = len(df)
     df = clean_data(df)
+    df = analyse_sentiment(df)
     st.success("Dataset uploaded successfully!")
 
     # Data cleaning summary
@@ -25,6 +27,37 @@ if uploaded_file is not None:
         st.write(f"Number of entries before cleaning: {before_cleaning}")
         st.write(f"Number of entries after cleaning: {len(df)}")
         st.write(f"Number of entries removed: {before_cleaning - len(df)}")
+
+    # Sentiment analysis results
+    st.subheader("Sentiment Analysis Results")
+    st.caption("Sentiment analysis results are potential indicators gathered from your dataset and should not be interepreted as definitive view of customer sentiment.")
+
+    # Sentiment distribution chart
+    fig, ax = plt.subplots()
+    sentiment_order = ['positive', 'neutral', 'negative']
+    distribution = df['sentiment_label'].value_counts().reindex(sentiment_order)
+    distribution.plot(kind='bar', ax=ax, color=['green', 'grey', 'red'])
+    ax.set_title("Sentiment Distribution")
+    ax.set_xlabel("Customer Sentiment")
+    ax.set_ylabel("Comment Count")
+    ax.set_xticklabels(sentiment_order, rotation=0)
+    st.pyplot(fig)
+    plt.close(fig)
+
+    # Sentiment trends over time
+    data_trends = df[['created_at', 'sentiment_score']].dropna()
+    data_trends = data_trends.sort_values('created_at')
+    data_trends = data_trends.set_index('created_at')
+    data_trends = data_trends.resample('ME').mean()
+    fig, ax = plt.subplots()
+    data_trends['sentiment_score'].plot(ax=ax)
+    ax.set_title("Sentiment Trends Over Time")
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Average Sentiment Score")
+    ax.axhline(y=0, color='grey', linestyle='--', label='Neutral sentiment', alpha=0.4)
+    ax.legend()
+    st.pyplot(fig)
+    plt.close(fig)
         
     # Display random sample of comments
     st.write("Random Sample of Comments:")
